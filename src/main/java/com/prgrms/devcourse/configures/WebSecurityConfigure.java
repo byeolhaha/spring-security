bf85ae23d92cf21dbc5ad1f3457108fde9073561
byeolhaha <j35635aa@gmail.com>
2023-08-01 14:52:54 +0900

    refactor : logout default 값 중복 코드 삭제

--- src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
+++ src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
@@ -35,10 +35,8 @@ public class WebSecurityConfigure {
                 )
                 .formLogin(login -> login.defaultSuccessUrl("/").permitAll())
                 .logout(logout -> logout
-                                .logoutUrl("/logout")
                                 .logoutSuccessUrl("/")
-                                .invalidateHttpSession(true)
-                                .deleteCookies("JSESSIONID")
+
                 );
 
         return http.build();
@@ -46,22 +44,17 @@ public class WebSecurityConfigure {
     }
 
     @Bean
-    UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+    UserDetailsService userDetailsService( ) {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(User.withUsername("user")
-                .password(passwordEncoder.encode("user123"))
+                .password("{noop}user123")
                 .roles("USER")
                 .build());
         manager.createUser(User.withUsername("admin")
-                .password(passwordEncoder.encode("admin123"))
+                .password("{noop}admin123")
                 .roles("ADMIN")
                 .build());
         return manager;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
     }
 
 }
