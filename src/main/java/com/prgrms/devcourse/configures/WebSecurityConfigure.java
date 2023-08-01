42753344fdc003f9995046a2d077cf0d4628e5d6
byeolhaha <j35635aa@gmail.com>
2023-08-01 17:47:13 +0900

    미션 2 완료

--- src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
+++ src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
@@ -3,13 +3,11 @@ package com.prgrms.devcourse.configures;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -35,16 +33,17 @@ public class WebSecurityConfigure {
                 )
                 .formLogin(login -> login.defaultSuccessUrl("/").permitAll())
                 .logout(logout -> logout
-                                .logoutSuccessUrl("/")
+                        .logoutSuccessUrl("/")
 
-                );
+                )
+                .requiresChannel(channel -> channel.anyRequest().requiresSecure()); // 모든 요청된 채널은 https 
 
         return http.build();
 
     }
 
     @Bean
-    UserDetailsService userDetailsService( ) {
+    UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(User.withUsername("user")
                 .password("{noop}user123")
